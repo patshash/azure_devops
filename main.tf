82e43c3cdc516c107174feddd0d77e5b4c5036f1
Patrick <pcarey@hashicorp.com>
2021-09-03 14:14:22 +1000

    update args for version

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 resource "azuredevops_project" "project" {
-  name                = "pcarey_prject"
-  description         = "my project terraform"
-  template_type_name  = "basic"
-  source_control_type = "Git"
-  visibility          = "private"
+  name               = "pcarey_prject"
+  description        = "my project terraform"
+  template_type_name = "Basic"
+  version_control    = "Git"
+  visibility         = "private"
 }
 
 
