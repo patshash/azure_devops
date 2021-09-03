5a2877bc1403b467e0cf08d23173a9e5fa489fe2
Patrick <pcarey@hashicorp.com>
2021-09-03 14:18:55 +1000

    add git import

--- main.tf
+++ main.tf
@@ -15,4 +15,12 @@ resource "azuredevops_project" "project" {
   visibility         = "private"
 }
 
-
+resource "azuredevops_git_repository" "repo" {
+  project_id = azuredevops_project.project.id
+  name       = "Import an Existing Repository"
+  initialization {
+    init_type   = "Import"
+    source_type = "Git"
+    source_url  = "https://github.com/patshash/azure_devops.git"
+  }
+}
